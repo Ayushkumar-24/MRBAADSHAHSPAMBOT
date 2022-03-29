8241b0ef8e1f65fa88e01f8f1b80c229b9a833bd
garwmishra <85062023+garwmishra@users.noreply.github.com>
2022-03-29 10:22:31 +0530

    Update Dm.py

--- BhaukaalBaba/plugins/Dm.py
+++ BhaukaalBaba/plugins/Dm.py
@@ -5,7 +5,7 @@ from telethon import events
 from telethon import functions, types
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 from resources.data import MightyX, RAID, MightyX1
-from BhaukaalBaba import Mig, Mig2, Mig3, Mig4, Mig5 , Mig6, Mig7, Mig8, Mig9, Mig10, Mig11, Mig12, Mig13, Mig14, Mig15, Mig16, Mig17, Mig18, Mig19, Mig20, Mig21, Mig22, Mig23, Mig24, Mig25, Mig26, Mig27, Mig28, Mig29, Mig30, Mig31, Mig32, Mig33, Mig34, Mig35, Mig36, Mig37, Mig38, Mig39, Mig40, SUDO_USERS
+from kaalspam import Mig, Mig2, Mig3, Mig4, Mig5 , Mig6, Mig7, Mig8, Mig9, Mig10, Mig11, Mig12, Mig13, Mig14, Mig15, Mig16, Mig17, Mig18, Mig19, Mig20, Mig21, Mig22, Mig23, Mig24, Mig25, Mig26, Mig27, Mig28, Mig29, Mig30, Mig31, Mig32, Mig33, Mig34, Mig35, Mig36, Mig37, Mig38, Mig39, Mig40, SUDO_USERS
 from .. import CMD_HNDLR as hl
 
 
@@ -67,7 +67,7 @@ async def _(e):
                 await e.reply(text, parse_mode=None, link_preview=None )
             else:            
                  message = str(Mighty[1])
-                 await e.reply("Message Delivered ✅")
+                 await e.reply("Message Delivered by kaalspam ✅")
                  await e.client.send_message(g, message)
                  await asyncio.sleep(0.3)
         elif e.reply_to_msg_id:             
@@ -81,7 +81,7 @@ async def _(e):
                 await e.reply(text, parse_mode=None, link_preview=None )
             else:
                 message = str(Mighty[0])
-                await e.reply("Message Delivered !! ✅")
+                await e.reply("Message Delivered by kaalspam!! ✅")
                 await e.client.send_message(g, message)
                 await asyncio.sleep(0.3)
 
@@ -147,7 +147,7 @@ async def dmraid(e):
                 await e.reply(text, parse_mode=None, link_preview=None )
             else:
                 counter = int(Mighty[0])
-                await e.reply("Dm Raid Started Successfully !! ✅")
+                await e.reply("Dm Raid Started Successfully by kaalspam!! ✅")
                 for _ in range(counter):
                     reply = random.choice(RAID)
                     caption = f"{reply}"
@@ -165,7 +165,7 @@ async def dmraid(e):
                 await e.reply(text, parse_mode=None, link_preview=None )
             else:
                 counter = int(Mighty[0])
-                await e.reply("Dm Raid Strated Successfully !! ✅")
+                await e.reply("Dm Raid Strated Successfully by kaalspam!! ✅")
                 for _ in range(counter):
                     reply = random.choice(RAID)
                     caption = f"{reply}"
